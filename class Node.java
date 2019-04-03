69279abc988c5ae73320833a8db512e99e38fe8c
ridhozamzami <49238510+ridhozamzami@users.noreply.github.com>
2019-04-03 20:30:41 +0700

    Sl

---
+++ class Node.java
@@ -0,0 +1,13 @@
+﻿public class Node {
+    
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public void tampil(){
+        System.out.print("{"+data+"}");
+    }
+}
